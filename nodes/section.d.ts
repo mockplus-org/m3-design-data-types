ec63cbf0e5f2cd3eebfb9faa62cab3dd69d4beac
Zhang WenXian <wishin_zhang@163.com>
2026-06-22 21:22:36 +0800

    调整包代码

--- nodes/section.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file 定义 M3 设计稿中的分区节点。
- */
-import { M3NodeTrait } from "./base";
-import { M3GroupTrait } from "./group";
-/**
- * 设计稿分区节点。
- */
-export type M3SectionNode = M3NodeTrait<"SECTION"> & M3GroupTrait;
--- resources/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @file 集中导出组件资源和样式资源类型。
- */
-export * from "./component";
-export * from "./styleResources";
